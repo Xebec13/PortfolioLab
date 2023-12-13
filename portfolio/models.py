791997bc457bde7992e4ba3378317a8fddacdafa
David <dhoesen@gmail.com>
2023-12-13 13:12:38 +0100

    Modele

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Institution(models.Model):
+    TYPE_CHOICES = [
+        ('fundacja', 'Fundacja'),
+        ('organizacja_pozarzadowa', 'Organizacja pozarządowa'),
+        ('zbiorka_lokalna', 'Zbiórka lokalna'),
+    ]
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES, default='fundacja')
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    quanity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=64)
+    city = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=64)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"Donation by {self.user.username if self.user else 'anonymous'}"
