d96a53d1e985c6ca6d468456d6343e1484eecaa5
David <dhoesen@gmail.com>
2023-12-13 14:25:05 +0100

    1.1

--- PortfolioLab/urls.py
+++ PortfolioLab/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from portfolio import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('', views.IndexView.as_view(), name='index'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self,request):
+        return render(request, "index.html")
+
+class LoginView(View):
+    def get(self,request):
+        return render(request, "login.html")
+
+class RegisterView(View):
+    def get(self,request):
+        return render(request, "register.html")
+
+class AddDonationView(View):
+    def get(self,request):
+        return render(request, )
